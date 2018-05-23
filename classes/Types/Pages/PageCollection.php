a3ef5a6b1474cca254c68d3819968c23a65eb116
Matias Griese <matias@trilbymedia.com>
2018-05-23 13:38:31 +0300

    Add PageCollection::getNextOrder()

--- classes/Types/Pages/PageCollection.php
+++ classes/Types/Pages/PageCollection.php
@@ -9,4 +9,28 @@ use Grav\Plugin\FlexObjects\FlexCollection;
  */
 class PageCollection extends FlexCollection
 {
+    public function getNextOrder()
+    {
+        $directory = $this->getFlexDirectory();
+
+        /** @var PageObject $last */
+        $collection = $directory->getCollection();
+        $keys = $collection->getStorageKeys();
+
+        // Assign next free order.
+        $last = null;
+        $order = 0;
+        foreach ($keys as $folder => $key) {
+            preg_match(PAGE_ORDER_PREFIX_REGEX, $folder, $test);
+            $test = $test[0] ?? null;
+            if ($test && $test > $order) {
+                $order = $test;
+                $last = $key;
+            }
+        }
+
+        $last = $collection[$last];
+
+        return sprintf('%d.', $last ? $last->value('order') + 1 : 1);
+    }
 }
