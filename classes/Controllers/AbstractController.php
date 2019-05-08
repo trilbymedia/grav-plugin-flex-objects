e325e1d608230a9608eecea6994f5771ceeec01f
Matias Griese <matias@trilbymedia.com>
2019-05-08 08:25:37 +0300

    Fixed fatal error when controller fails on an error with bad code

--- classes/Controllers/AbstractController.php
+++ classes/Controllers/AbstractController.php
@@ -291,6 +291,11 @@ abstract class AbstractController implements RequestHandlerInterface
      */
     public function createErrorResponse(\Exception $e): ResponseInterface
     {
+        $validCodes = [
+            400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418,
+            422, 423, 424, 425, 426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 511
+        ];
+
         if ($e instanceof RequestException) {
             $code = $e->getHttpCode();
             $reason = $e->getHttpReason();
@@ -299,9 +304,13 @@ abstract class AbstractController implements RequestHandlerInterface
             $reason = null;
         }
 
+        if (!in_array($code, $validCodes, true)) {
+            $code = 500;
+        }
+
         $message = $e->getMessage();
         $response = [
-            'code' => $e->getCode() ?: 500,
+            'code' => $code,
             'status' => 'error',
             'message' => $message
         ];
