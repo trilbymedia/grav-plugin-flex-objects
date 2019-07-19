de1bf4de8b0e0fbca34edf5d241381f793387cde
Matias Griese <matias@trilbymedia.com>
2019-07-19 13:44:15 +0300

    Add Page/Folder have pre-defined values based on the parent

--- classes/Admin/AdminController.php
+++ classes/Admin/AdminController.php
@@ -408,7 +408,7 @@ class AdminController
 
             /** @var ServerRequestInterface $request */
             $request = $grav['request'];
-            $postAction = $request->getParsedBody()['data']['_post_entries_save'] ?? 'list';
+            $postAction = $request->getParsedBody()['data']['_post_entries_save'] ?? 'edit';
 
             /** @var FlexForm $form */
             $form = $this->getForm($object);
