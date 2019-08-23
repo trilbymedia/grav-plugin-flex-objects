d8fed63f30b5640487f1df8f424dc659af00b5fb
Matias Griese <matias@trilbymedia.com>
2019-08-23 21:38:12 +0300

    Removed some unneeded code

--- classes/Admin/AdminController.php
+++ classes/Admin/AdminController.php
@@ -777,20 +777,6 @@ class AdminController
             if (!$this->validateNonce()) {
                 $e = new RequestException($this->getRequest(), 'Page Expired', 400);
 
-                $accept = $this->getAccept(['application/json', 'text/html']);
-                if ($accept === 'text/html') {
-                    if (!$this->action) {
-                        if ($this->id) {
-                            $this->action = 'edit';
-                            $params[] = $this->id;
-                        } else {
-                            $this->action = 'list';
-                        }
-                    }
-                    $this->setMessage($e->getMessage(), 'error');
-                    return false;
-                }
-
                 $this->close($this->createErrorResponse($e));
             }
             $method = $this->task_prefix . ucfirst(str_replace('.', '', $this->task));
