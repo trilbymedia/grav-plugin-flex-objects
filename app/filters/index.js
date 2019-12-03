3c9a85f7661d4cfe07f1d262d11ff5f3976aeb08
Djamil Legato <djamil@djamil.it>
2019-12-03 13:46:51 -0800

    Added logic for panel toggle

--- app/filters/index.js
+++ app/filters/index.js
@@ -1,4 +1,5 @@
 import '../utils/indeterminate';
+import './panel';
 import { ReLoad } from '../columns';
 
 document.addEventListener('click', (event) => {
@@ -31,16 +32,3 @@ document.addEventListener('click', (event) => {
         return false;
     }
 });
-
-/*
-document.addEventListener('input', (event) => {
-    if ((event.detail || event).target.tagName === 'INPUT' && (event.detail || event).target.closest('#pages-filters')) {
-        event.preventDefault();
-
-        // const activeItem = (FlexPagesInstance.finder.findLastActive() || { item: [{ _item: null }] }).item[0]._item;
-
-        ReLoad();
-        /!* FlexPagesInstance.dataLoad(null, (data) => FlexPagesInstance.finder.goTo(data, getInitialRoute()), getFilters());*!/
-    }
-});
-*/
