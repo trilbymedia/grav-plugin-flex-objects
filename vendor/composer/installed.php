958a61590587128f7263dca526c5ae7edde80511
Matias Griese <matias@trilbymedia.com>
2021-06-10 09:32:15 +0300

    Composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7bfde54d9213f68039891d2076bbab21009bde7c',
+    'name' => 'getgrav/grav-plugin-flex-objects',
+  ),
+  'versions' => 
+  array (
+    'getgrav/grav-plugin-flex-objects' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7bfde54d9213f68039891d2076bbab21009bde7c',
+    ),
+  ),
+);
